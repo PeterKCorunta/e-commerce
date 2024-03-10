ebbd90f8d3f900d0d378aa875b4a9cda8289bb73
kelechiorunta <oruntakelechi86@gmail.com>
2024-03-10 14:47:27 +0100

    My testimonial array objs

---
+++ src/Components/testimonialpics.js
@@ -0,0 +1,15 @@
+import pic1 from '../Components/TestimonialPics/picimg1.jpg'
+import pic2 from '../Components/TestimonialPics/picimg2.jpg'
+import pic3 from '../Components/TestimonialPics/picimg3.jpg'
+import pic4 from '../Components/TestimonialPics/picimg4.jpg'
+import pic5 from '../Components/TestimonialPics/picimg5.jpeg'
+import pic6 from '../Components/TestimonialPics/picimg1.jpg'
+
+export const testimonialpics = [
+    {id:1, pic: pic1, desc:"It's a wonderful app and I'm sure the best is even yet to come!", author:"Carl Jens"},
+    {id:2, pic: pic2, desc:"Second time using this app, and it's cool!!!", author:"Carl Jens"},
+    {id:3, pic: pic3, desc:"Third time using this app, and it's cool!!!", author:"Fredrick Cole"},
+    {id:4, pic: pic4, desc:"Fourth time using this app, and it's cool!!!", author:"Simon Halo"},
+    {id:5, pic: pic5, desc:"Fifth time using this app, and it's cool!!!", author:"Gene Jackman"},
+    {id:6, pic: pic6, desc:"Sixth time using this app, and it's cool!!!", author:"Paul Jones"},
+]
